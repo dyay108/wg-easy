8e43b88a8269ce95aaf7deffe8a3c3a6f65a40b1
dyay108 <17689521+dyay108@users.noreply.github.com>
2026-01-20 13:20:31 -0600

    ui work to allow client as exit node

--- src/app/pages/clients/[id].vue
+++ src/app/pages/clients/[id].vue
@@ -92,10 +92,15 @@
               :label="$t('client.egressEnabled')"
               :description="$t('client.egressEnabledDesc')"
             />
-            <div v-if="data.egressEnabled">
-              <label for="egressDevice" class="mb-2 block text-sm font-medium text-gray-700 dark:text-neutral-300">
-                {{ $t('client.egressDevice') }}
-              </label>
+            <template v-if="data.egressEnabled">
+              <div class="flex items-center">
+                <FormLabel for="egressDevice">
+                  {{ $t('client.egressDevice') }}
+                </FormLabel>
+                <BaseTooltip :text="$t('client.egressDeviceDesc')">
+                  <IconsInfo class="size-4" />
+                </BaseTooltip>
+              </div>
               <select
                 id="egressDevice"
                 v-model="data.egressDevice"
@@ -106,9 +111,26 @@
                   {{ device }}
                 </option>
               </select>
-              <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
-                {{ $t('client.egressDeviceDesc') }}
-              </p>
+            </template>
+            <FormSwitchField
+              id="isExitNode"
+              v-model="data.isExitNode"
+              :label="$t('client.isExitNode')"
+              :description="$t('client.isExitNodeDesc')"
+            />
+            <div v-if="data.isExitNode" class="col-span-full space-y-4 rounded-lg border-2 border-gray-100 p-4 dark:border-neutral-800">
+              <div>
+                <p class="mb-2 text-sm font-medium text-gray-700 dark:text-neutral-300">
+                  {{ $t('client.isExitNodePostUp') }}
+                </p>
+                <pre class="overflow-x-auto rounded-lg border border-gray-200 bg-gray-50 p-3 text-xs text-gray-800 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-200">iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth+ -j MASQUERADE</pre>
+              </div>
+              <div>
+                <p class="mb-2 text-sm font-medium text-gray-700 dark:text-neutral-300">
+                  {{ $t('client.isExitNodePostDown') }}
+                </p>
+                <pre class="overflow-x-auto rounded-lg border border-gray-200 bg-gray-50 p-3 text-xs text-gray-800 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-200">iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth+ -j MASQUERADE</pre>
+              </div>
             </div>
           </FormGroup>
           <FormGroup v-if="globalStore.information?.isAwg">
